1a2f894a530abfdf7944012a3ecfa617351aad2d
Harmedino <adamsahmed191@gmail.com>
2023-11-16 17:26:21 +0100

    welcome header started

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,4 +1,13 @@
-<div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+<div class=" bg-red-400 h-24 w-full">
+    <div>
+        <h4>Kolobank</h4>
+        <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Blog</li>
+            <li>contact Us</li>
+        </ul>
+    </div>
     @auth
         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Dashboard</a>
     @else
@@ -9,3 +18,4 @@
         @endif
     @endauth
 </div>
+{{-- sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10 --}}
