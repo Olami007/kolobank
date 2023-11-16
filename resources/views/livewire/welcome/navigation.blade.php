ddaa0734cad141abfff339e3fb5b2ec12b8fd49c
Harmedino <adamsahmed191@gmail.com>
2023-11-16 17:45:17 +0100

    design moving

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,21 +1,26 @@
-<div class=" bg-red-400 h-24 w-full">
-    <div>
-        <h4>Kolobank</h4>
-        <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Blog</li>
-            <li>contact Us</li>
+<div class="flex justify-around items-center w-full h-24">
+    <div class="flex items-center">
+        <img src="{{'images/kudi_logo.png'}}" alt="logo" class="mr-4 h-16 w-auto bg-gray-100 dark:bg-gray-900">
+        <h4 class="">Kolobank</h4>
+    </div>
+
+    <ul class="flex justify-around items-center w-72">
+        <li class="">Home</li>
+        <li class="">About Us</li>
+        <li class="">Blog</li>
+        <li class="">
+            <a href="#" class="hover:text-gray-300">Contact Us</a>
+        </li>
+    </ul>
+
+    <div x-data="{ isOpen: false }" class="relative inline-block">
+        <span @click="isOpen = !isOpen" class="cursor-pointer ">Sell Bitcoin/Giftcard</span>
+        <ul x-show="isOpen" @click.away="isOpen = false" class="absolute  z-10 pt-1">
+            <li><a class="rounded-t hover:bg-gray-800 py-2 px-4 block whitespace-no-wrap " href="#">Sell Bitcoin</a></li>
+            <li><a class="hover:bg-gray-800 py-2 px-4 block whitespace-no-wrap" href="#">Sell Giftcard</a></li>
         </ul>
     </div>
-    @auth
-        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Dashboard</a>
-    @else
-        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Log in</a>
 
-        @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Register</a>
-        @endif
-    @endauth
+    <button class="bg-blue-500  py-2 px-4 rounded-full focus:outline-none focus:ring focus:border-blue-300 transition-all duration-300 ease-in-out hover:bg-blue-600">Login</button>
 </div>
 {{-- sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10 --}}
