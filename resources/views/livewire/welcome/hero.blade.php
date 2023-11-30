8a6e0bbff4cbd67923e9e5ed0cd6f3c3afec2242
Harmedino <adamsahmed191@gmail.com>
2023-11-30 22:49:00 +0100

    moving well

--- resources/views/livewire/welcome/hero.blade.php
+++ resources/views/livewire/welcome/hero.blade.php
@@ -1,13 +1,19 @@
-<div class="hero-section w-full text-white">
-   <div class="container mx-auto">
-    <div class="flex justify-center items-start">
-        <div class="w-1/2 flex flex-col items-center justify-center h-full">
-        <h3>We provide easy solution to exchange your <span>Bitcoin / Giftcard for money </span></h3>
-    <p>Kolobank is a platform for trading your Bitcoin and Giftcard at the best rate, why not give a trial</p>
-<button>GET STARTED</button>
-</div>
-        <div class="w-1/2">
-            <img src="{{'images\88599100-bitcoin-transfer-wallet-hand-holds-smart-phone-with-send-bitcoins-on-blue-background-vector-flat-removebg-preview.png'}}" alt="Logo" class="w-auto mb-4"></div>
+<div class="hero-section w-full text-white md:h-full mb-8">
+    <div class="container mx-auto">
+        <div class="flex flex-col md:flex-row items-center justify-center md:justify-between">
+            <div class="w-full md:w-1/2 flex flex-col items-center justify-center md:h-full p-6 md:p-12">
+                <h3 class="text-2xl md:text-4xl font-bold text-white">
+                    We provide easy solutions to exchange your <span class="text-3xl md:text-5xl font-bold">Bitcoin / Giftcard for money</span>
+                </h3>
+                <p class="text-sm md:text-lg mt-4">
+                    Kolobank is a platform for trading your Bitcoin and Giftcard at the best rate. Why not give it a try?
+                </p>
+                <button class="bg-white text-purple-600 rounded-full px-6 py-2 mt-4">GET STARTED</button>
+            </div>
+            <div class="w-full md:w-1/2">
+                <img src="{{'images\88599100-bitcoin-transfer-wallet-hand-holds-smart-phone-with-send-bitcoins-on-blue-background-vector-flat-removebg-preview.png'}}" alt="Logo" class="w-full h-auto">
+            </div>
+        </div>
     </div>
-   </div>
 </div>
+
