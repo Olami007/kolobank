10cbcc6f6c03fba903760c68726ecc3a95684464
Harmedino <adamsahmed191@gmail.com>
2023-11-30 22:14:46 +0100

    hero section

--- resources/views/livewire/welcome/hero.blade.php
+++ resources/views/livewire/welcome/hero.blade.php
@@ -1,1 +1,18 @@
-<h1>hello am hero page</h1>
+<div class="hero-section mb-12">
+    <h1 class="text-4xl md:text-6xl font-bold">Hello, I am Hero Page</h1>
+
+    <div class="content-container">
+        <p class="text-lg md:text-xl">Some text content here...</p>
+
+        <div class="image-container">
+            <!-- Add your image here -->
+            <img src="{{ asset('images/your-image.jpg') }}" alt="Image" class="w-full max-w-md mx-auto rounded-md">
+        </div>
+
+        <button class="text-purple-800">Get Started</button>
+    </div>
+
+    <div class="wave-container">
+        <div class="wave"></div>
+    </div>
+</div>
