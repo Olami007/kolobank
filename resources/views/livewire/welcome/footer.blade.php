07d53196c823c36fc9fb654b1e40121a9ab9569a
Harmedino <adamsahmed191@gmail.com>
2023-11-30 22:07:28 +0100

    tried with footer

--- resources/views/livewire/welcome/footer.blade.php
+++ resources/views/livewire/welcome/footer.blade.php
@@ -1,11 +1,11 @@
    {{-- Footer --}}
    <footer class="bg-black h-full">
-    <div class="container mx-auto text-white py-12">
+    <div class="container mx-auto text-white py-12 ">
 
-        <div class="flex flex-wrap justify-around md:items-start items-center">
+        <div class="flex flex-wrap justify-center items-center">
 
             <!-- First Column -->
-            <div class="w-full sm:w-1/2 lg:w-1/3 mb-8">
+            <div class="w-full sm:w-2/3 lg:w-1/3 mb-8 items-center p-4 flex flex-col justify-center">
                 <img src="{{'images/kudi_logo.png'}}" alt="Logo" class="w-12 mb-4">
                 <p class="text-sm md:text-base leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sunt ut officiis beatae magni ipsam veniam deserunt incidunt nesciunt libero!</p>
                 <ul class="flex items-center justify-around space-x-5 mt-4 w-2/4">
@@ -18,7 +18,7 @@
             </div>
 
             <!-- Second Column -->
-            <div class="w-full sm:w-1/2 lg:w-1/3 mb-8">
+            <div class="w-full sm:w-2/3 lg:w-1/3 mb-8 p-4 flex flex-col items-center">
                 <h4 class="text-lg font-bold mb-4">Company Links</h4>
                 <ul class="text-sm md:text-base">
                     <li><a href="#">Home</a></li>
@@ -29,7 +29,7 @@
             </div>
 
             <!-- Third Column -->
-            <div class="w-full sm:w-1/2 lg:w-1/4 mb-8">
+            <div class="w-full sm:w-2/3 lg:w-1/4 mb-8 p-4 flex flex-col  items-center">
                 <h5 class="text-lg font-bold mb-4">Contact us</h5>
                 <p class="mb-2">070494949</p>
                 <p class="mb-2">070494949</p>
@@ -37,28 +37,29 @@
             </div>
 
         </div>
+        <div class="flex justify-center mt-16 border-red-400 px-0 sm:items-center  ">
+            <div class="text-center text-sm   sm:text-left">
+                <div class="flex items-center gap-4">
+                    <a href="https://github.com/Olami007"
+                        class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            class="-mt-px mr-1 w-5 h-5 stroke-gray-400 dark:stroke-gray-600 group-hover:stroke-gray-600 dark:group-hover:stroke-gray-400">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        </svg>
+                        Creator
+                    </a>
+                </div>
+            </div>
 
+            <div class="ml-4 pl-1 text-center text-sm   sm:text-right sm:ml-0">
+                Olami_bells & Harmedino
+            </div>
+        </div>
     </div>
 </footer>
 
 
 
-{{-- <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between absolute bottom-4">
-    <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
-        <div class="flex items-center gap-4">
-            <a href="https://github.com/Olami007"
-                class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    class="-mt-px mr-1 w-5 h-5 stroke-gray-400 dark:stroke-gray-600 group-hover:stroke-gray-600 dark:group-hover:stroke-gray-400">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                </svg>
-                Creator
-            </a>
-        </div>
-    </div>
 
-    <div class="ml-4 pl-1 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
-        Olami_bells & Harmedino
-    </div>
-</div> --}}
+{{-- --}}
