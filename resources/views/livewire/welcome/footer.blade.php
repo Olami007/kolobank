1f4ab5531bb20ac7b88bdcd94bd4bd90efb55c82
Harmedino <adamsahmed191@gmail.com>
2023-11-30 21:37:47 +0100

    footer started

--- resources/views/livewire/welcome/footer.blade.php
+++ resources/views/livewire/welcome/footer.blade.php
@@ -1,10 +1,10 @@
    {{-- Footer --}}
    <footer class="bg-black h-[30vh]">
-    <div class="container mx-auto flex flex-ro h-full justify-between">
+    <div class="container mx-auto">
 
         <!-- First Row: Logo and Social Media Links -->
-        <div class="flex items-center flex-col justify-between py-4">
-            <div class="flex items-center">
+        <div class="flex items-center justify-between py-4">
+            <div>
                 <!-- Your logo goes here -->
                 <img src="path/to/your/logo.png" alt="Logo" class="h-8 w-8 mr-4">
                 <!-- Lorem 40 words -->
@@ -22,8 +22,9 @@
         </div>
 
         <!-- Second Row: Company Links -->
-        <div class="flex justify-center">
-            <ul class="flex items-center text-white">
+        <div class="py-2">
+            <h4 class="text-white">Company Links</h4>
+            <ul class="text-white">
                 <li class="mx-4"><a href="#">Home</a></li>
                 <li class="mx-4"><a href="#">About Us</a></li>
                 <li class="mx-4"><a href="#">Sell</a></li>
@@ -33,33 +34,30 @@
         </div>
 
         <!-- Third Row: Contact Information -->
-        <div class="flex justify-between py-4">
-            <div>
-                <p class="text-white">Contact Us:</p>
-                <p class="text-white">Phone: +1234567890</p>
-                <p class="text-white">Email: info@example.com</p>
-            </div>
-
-            <!-- Social Media Links (Inline) -->
-            <div class="flex items-center">
-                <a href="https://github.com/Olami007" class="group inline-flex items-center text-white hover:text-gray-700">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        class="-mt-px mr-1 w-5 h-5 stroke-gray-400 group-hover:stroke-gray-600">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                    </svg>
-                    Creator
-                </a>
-            </div>
+        <div class="py-2">
+            <p class="text-white">Contact Us:</p>
+            <p class="text-white">Phone: +1234567890</p>
+            <p class="text-white">Email: info@example.com</p>
         </div>
 
         <!-- Inline Text -->
+        <div class="flex items-center">
+            <a href="https://github.com/Olami007" class="group inline-flex items-center text-white hover:text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    class="-mt-px mr-1 w-5 h-5 stroke-gray-400 group-hover:stroke-gray-600">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                </svg>
+                Creator
+            </a>
+        </div>
+
         <div class="ml-4 pl-1 text-center text-sm text-gray-500">
             Olami_bells & Harmedino
         </div>
-
     </div>
 </footer>
+
 
 {{-- <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between absolute bottom-4">
     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
