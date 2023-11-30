8efac9a1f197ade6c2608b7261128273e72c07e6
Harmedino <adamsahmed191@gmail.com>
2023-11-30 22:44:07 +0100

    hero section

--- resources/views/livewire/welcome/hero.blade.php
+++ resources/views/livewire/welcome/hero.blade.php
@@ -1,18 +1,13 @@
-<div class="hero-section mb-12">
-    <h1 class="text-4xl md:text-6xl font-bold">Hello, I am Hero Page</h1>
-
-    <div class="content-container">
-        <p class="text-lg md:text-xl">Some text content here...</p>
-
-        <div class="image-container">
-            <!-- Add your image here -->
-            <img src="{{ asset('images/your-image.jpg') }}" alt="Image" class="w-full max-w-md mx-auto rounded-md">
-        </div>
-
-        <button class="text-purple-800">Get Started</button>
+<div class="hero-section w-full text-white">
+   <div class="container mx-auto">
+    <div class="flex justify-center items-start">
+        <div class="w-1/2 flex flex-col items-center justify-center h-full">
+        <h3>We provide easy solution to exchange your <span>Bitcoin / Giftcard for money </span></h3>
+    <p>Kolobank is a platform for trading your Bitcoin and Giftcard at the best rate, why not give a trial</p>
+<button>GET STARTED</button>
+</div>
+        <div class="w-1/2">
+            <img src="{{'images\88599100-bitcoin-transfer-wallet-hand-holds-smart-phone-with-send-bitcoins-on-blue-background-vector-flat-removebg-preview.png'}}" alt="Logo" class="w-auto mb-4"></div>
     </div>
-
-    <div class="wave-container">
-        <div class="wave"></div>
-    </div>
+   </div>
 </div>
